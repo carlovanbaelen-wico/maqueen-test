d600ce084fabefbe4b96dcdd6754e57b5e73cc50
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2020-10-05 21:14:13 +0200

    Gezichtshekenning Tom Scarlet

--- main.ts
+++ main.ts
@@ -1,5 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
     huskylens.writeOSD("Carlo", 150, 120)
 })
+input.onButtonPressed(Button.B, function () {
+    huskylens.clearOSD()
+})
+let Id = 0
 DFRobotMaqueenPlus.I2CInit()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+basic.forever(function () {
+    huskylens.request()
+    Id = huskylens.readBox_s(Content3.ID)
+    if (Id == 1) {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+        basic.pause(1000)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        basic.pause(1000)
+    } else if (Id == 7) {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+        basic.pause(1000)
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        basic.pause(1000)
+    } else {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+    }
+})
