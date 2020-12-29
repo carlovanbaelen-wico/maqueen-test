32e637aa2ec905ded68814b3961ab2772c66c7ed
carlovanbaelen-wico <67262093+carlovanbaelen-wico@users.noreply.github.com>
2020-12-29 16:13:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ input.onButtonPressed(Button.B, function () {
     huskylens.clearOSD()
 })
 let Id = 0
+radio.setGroup(2)
 DFRobotMaqueenPlus.I2CInit()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 basic.forever(function () {
@@ -12,15 +13,20 @@ basic.forever(function () {
     Id = huskylens.readBox_s(Content3.ID)
     if (Id == 1) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+        radio.sendNumber(9)
         basic.pause(1000)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        radio.sendNumber(11)
         basic.pause(1000)
     } else if (Id == 7) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+        radio.sendNumber(15)
         basic.pause(1000)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        radio.sendNumber(13)
         basic.pause(1000)
     } else {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+        radio.sendNumber(20)
     }
 })
